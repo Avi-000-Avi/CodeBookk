ad1d60ae40785f7f0976f0318570980fb4439e2c
Avi-000-Avi <atoppo267@gmail.com>
2021-11-06 23:32:47 +0530

    Images added

--- codebook/src/index.tsx
+++ codebook/src/index.tsx
@@ -9,6 +9,7 @@ const App = () => {
     const iframe = useRef<any>();
     const [input,setInput] = useState('');
     const [code,setCode] = useState('');
+
     const startService = async () => {
         ref.current = await esbuild.startService({
           worker: true,
@@ -47,30 +48,38 @@ const App = () => {
        iframe.current.contentWindow.postMessage(result.outputFiles[0].text,'*');
     }
 
-    const html =`
+      const html = `
     <html>
-        <head></head>
-        <body>
-            <div id="root"></div>
-            <script>
-                window.addEventListener('message',(event) => {
-                    eval(event.data);
-                },false);
-            </script>
-        </body>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+                eval(event.data);
+            } catch(err) {
+                const root = document.querySelector('#root');
+                root.innerHTML = '<div style = color:red;><h4>Runtime Error</h4>' + err + '</div>'
+            }
+          }, false);
+        </script>
+      </body>
     </html>
-    `;
+  `;
 
-    return <div>
-        <textarea value = {input} onChange={e => setInput(e.target.value)}></textarea>
-        <div>
-            <button onClick={onClick}> Submit</button>
-        </div>
-        <pre>{code}</pre>
-        {/*Used to show one html  document in another*/}
-        <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
-    </div>;
-
+      return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+    </div>
+  );
 };
 
 ReactDom.render(
