ba63de5474b89d7a40f2bf6af4c18e5c81c03691
Avinash Toppo <atoppo267@gmail.com>
2021-11-15 21:29:06 +0530

    Draggable Resizable Components done

--- codebook/src/components/code-cell.tsx
+++ codebook/src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import {useState, } from 'react';
+import {useState,useEffect } from 'react';
 import CodeEditor from './codeEditor';
 import Preview from './preview';
 import bundle from '../bundler';
@@ -8,17 +8,26 @@ const CodeCell = () => {
     const [code,setCode] = useState('');
     const [input,setInput] = useState('');
  
-    const onClick = async () =>{ 
+    useEffect(() => {
+      const timer = setTimeout(async () => {
         const output = await bundle(input);
         setCode(output);
-    }
+      },750);
+
+      return () => {
+        clearTimeout(timer);
+      }
+    }, [input])
+
       return (
     <Resizable direction="vertical">
       <div style={{height:'100%',display:'flex',flexDirection:'row'}}>
+      <Resizable direction="horizontal">
       <CodeEditor 
       initialValue="const a =1;" 
       onChange = {(value) => setInput(value)}
       />
+      </Resizable>
        <Preview code ={code}/>
        </div>
     </Resizable>
